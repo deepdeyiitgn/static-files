0a1f2dab2b551761a49db1547744f9c39db8f546
Deep Dey <deydeep@users.noreply.huggingface.co>
2026-03-30 15:56:08 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # 1. Tere ping karne wale bot ko background mein start kar rahe hain
 python bot.py &
 
-# 2. Tera main app foreground mein chalega
-python main.py
+# 2. Main FastAPI app ko Uvicorn ke through port 7860 pe start kar rahe hain
+uvicorn main:app --host 0.0.0.0 --port 7860
